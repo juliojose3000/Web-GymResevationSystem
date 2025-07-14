a52874cc675366111728e9ea3416c29ffffdc790
Julidev <juliodev9810@gmail.com>
2025-07-14 08:44:02 -0600

    feat -> Update project to angular 16

--- src/app/guards/admin.guard.ts
+++ src/app/guards/admin.guard.ts
@@ -1,10 +1,10 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from "@angular/router";
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router";
 import { Observable } from "rxjs";
 
 
 @Injectable()
-export class AdminGuard implements CanActivate {
+export class AdminGuard  {
 
 
     private hasAccess: boolean;
